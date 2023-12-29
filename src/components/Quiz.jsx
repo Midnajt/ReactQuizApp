ceb6df228372969f2dd504cede75a91c3e81d424
Marcin K <m.krzysztoszek@cermotor.com.pl>
2023-12-29 15:52:17 +0100

    Displaying questions added to Quiz compone

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
 
+import QUESTIONS from "../questions.js";
+
 function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUsersAnswers] = useState([]);
 
-  return <p>Currently active question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUsersAnswers((prevUsersAnswers) => {
+      return [...prevUsersAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} data-item={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
